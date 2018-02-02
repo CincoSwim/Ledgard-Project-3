a74700832f9cc41ea8f646bcaaa806b88e2cc896
CincoSwim <35818836+CincoSwim@users.noreply.github.com>
2018-02-02 16:15:17 -0500

    Added the actual meat to the project.

---
+++ src/Adder.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Adder {
+    /* Written by Christopher Pucko
+       Computes the sum of a 3-digit number's digits by using Modulo division.
+       i.e. An input of the number 733 would output 13 as the sum
+     */
+    public static void main(String[] args) {
+        Scanner keyin = new Scanner(System.in);
+        int sum;
+        int numInput;
+
+        sum = 0;
+
+        try {
+            System.out.print("Enter an integer: ");
+            numInput = keyin.nextInt();
+            while (numInput > 0) {
+                sum = sum + numInput % 10;
+                numInput = numInput / 10;
+            }
+            System.out.println("The sum of the digits is: " + sum);
+        } catch (InputMismatchException e) {
+            System.out.println("This is not a number. Please try again later.");
+        }
+    }
+}
