35497b26f8dde8eaba819c98fb84f8703618bb42
CincoSwim <35818836+CincoSwim@users.noreply.github.com>
2018-02-02 16:36:03 -0500

    Added PiApprox, part B to project 3

--- src/PiApprox.java
+++ src/PiApprox.java
@@ -1,2 +1,13 @@
 public class PiApprox {
+    /*
+    Written By Christopher Pucko
+    Prints an approximate value of Pi, after calculating the value of Pi up to a specified point.
+     */
+    public static void main(String[]args){
+        double aproxPi;
+
+        aproxPi = (4.0 * (1.0 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11) + (1.0/13)));
+
+        System.out.println("The Approximate Value of Pi is " + aproxPi);
+    }
 }
