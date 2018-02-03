3c06e54d95790e6a147f25dad51977730fcc1115
CincoSwim <35818836+CincoSwim@users.noreply.github.com>
2018-02-02 19:25:01 -0500

    Added PiApprox, part B to project 3

---
+++ src/WindChill.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class WindChill {
+    /*
+    Written by Christopher Pucko
+    Prints the Wind Chill Index, after temperature and wind speed are supplied.
+    Temperature is in Fahrenheit, Wind Speed is in Miles per Hour
+     */
+    public static void main(String[] args) {
+        Scanner inputNum = new Scanner(System.in);
+
+        double windSpeed;
+        double temp;
+        double chillIndex;
+        double windExp;
+
+        try {
+            System.out.print("Enter temperature(Fahrenheit): ");
+            temp = inputNum.nextDouble();
+
+            System.out.print("Enter wind speed(mph): ");
+            windSpeed = inputNum.nextDouble();
+
+            windExp = Math.pow(windSpeed, 0.16);
+
+            chillIndex = 35.74 + 0.6215*temp - 35.75 * windExp + 0.4275 * temp * windExp;
+
+            System.out.println("The wind chill index is " + chillIndex);
+
+
+        }
+
+        catch(InputMismatchException ex){
+            System.out.println("Either the Wind Speed or Temperature you've entered were not numbers.");
+            System.out.println("Please Try again later.");
+        }
+    }
+}
+
