cdb941ccf6c128b40bc7ed40ea0864512fdc4a94
CincoSwim <cgpucko@gmail.com>
2018-02-06 22:23:00 -0500

    Casts Wind Chill as an int, and started work on a slope length calculator

--- src/SlopeLengthCalculator.java
+++ src/SlopeLengthCalculator.java
@@ -5,7 +5,6 @@ public class SlopeLengthCalculator {
     Written by Christopher Pucko
     Outputs the numerical distance between two points after prompting an input of 2 coordinate points.
      */
-
     public static void main(String[] args) {
         double x1;
         double y1;
@@ -14,7 +13,6 @@ public class SlopeLengthCalculator {
         double dist;
 
         Scanner numInput = new Scanner(System.in);
-
 
         try {
             System.out.print("Enter x1 and y1:");
@@ -27,14 +25,11 @@ public class SlopeLengthCalculator {
             x2 = numInput.nextDouble();
             y2 = numInput.nextDouble();
 
-
             dist = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
 
             System.out.println("The distance is " + (float) dist);
         } catch (InputMismatchException ex) {
             System.out.print("This input was not a number. Please try again.");
-
-
         }
     }
 }
